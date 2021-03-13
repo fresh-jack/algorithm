cfff14b375e6d12af1ddc17607cc889f823d0fed
wgs <weiguoshuai@xiaozhu.com>
2021-03-13 17:38:26 +0800

    list

--- list/list.go
+++ list/list.go
@@ -6,7 +6,67 @@ type Node struct {
 	Next *Node
 }
 
-// ReverseList ...
-func ReverseList(head *Node) *Node {
-	return nil
+/*
+	reverse List
+*/
+func reverseList(head *Node) *Node {
+	var prev *Node
+	cur := head
+	for cur != nil {
+		temp := cur.Next
+		cur.Next = prev
+
+		cur = temp
+	}
+
+	return prev
+}
+
+/*
+	middleNode
+*/
+func middleNode(head *Node) *Node {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+/*
+	remove duplicate nodes
+*/
+func removeDuplicateNodes(head *Node) *Node {
+	cur := head
+	for cur != nil {
+		temp := cur
+		for temp.Next != nil {
+			if temp.Next.Val == cur.Val {
+				temp.Next = temp.Next.Next
+			} else {
+				temp = temp.Next
+			}
+		}
+		cur = cur.Next
+	}
+
+	return head
+}
+
+func removeDuplicateNodes2(head *Node) *Node {
+	if head != nil {
+		cur := head
+		m := map[int]struct{}{head.Val: struct{}{}}
+		for cur.Next != nil {
+			if _, ok := m[cur.Next.Val]; !ok {
+				m[cur.Next.Val] = struct{}{}
+				cur = cur.Next
+			} else {
+				cur.Next = cur.Next.Next
+			}
+		}
+	}
+	return head
 }
